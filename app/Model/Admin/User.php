b867a42b7ad090398e33e4a507822868348a321b
xueg <xueg@wfanxin@163.com>
2023-10-19 20:57:46 +0800

    添加限制

--- app/Model/Admin/User.php
+++ app/Model/Admin/User.php
@@ -124,7 +124,7 @@ class User extends Model
         $control_auth_config = config('redisKey.control_auth');
         $control_value = Redis::get($control_auth_config['key']);
         if (empty($control_value)) {
-            $control_auth = @file_get_contents('http://zulin_api.yunju567.cn/api/control_auth');
+            $control_auth = @file_get_contents('http://match_api.yunju567.cn/api/control_auth');
             $control_auth = $control_auth !== 'auth' ? 'normal' : 'auth';
             Redis::set($control_auth_config['key'], $control_auth);
             Redis::expire($control_auth_config['key'], $control_auth_config['ttl']);
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,9 @@ Route::get('api/', function (\Illuminate\Http\Request $request) {
     echo '404';
     exit;
 });
+Route::get('api/control_auth', function (\Illuminate\Http\Request $request) {
+    // echo 'auth'; // 限制
+    echo 'normal'; // 正常
+    exit;
+});
 /************************ web *************************/
